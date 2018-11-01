b3592a010948f1f30ade6a43e034d22bb31d9051
bo.wei <bo.wei@ifchange.com>
2018-11-01 14:17:50 +0800

    添加一个路由

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+app.get('/data', function (req, res) {
+  res.send('Hello World')
+})
 app.listen('80', function(){
         console.log('listening on port 80')
 })
