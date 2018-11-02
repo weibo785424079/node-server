22277d741c289c0dd104a19e1409c8629810604e
bo.wei <bo.wei@ifchange.com>
2018-11-02 15:37:21 +0800

    修改dockerfile

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 app.get('/data', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello weibo')
 })
 app.listen(7100, function(){
         console.log('listening on port 7100')
