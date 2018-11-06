2f61eced7f75dad88f83f840fa6ac875a4533ce1
bo.wei <bo.wei@ifchange.com>
2018-11-06 16:44:21 +0800

    尝试映射文件夹

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 app.get('/data', function (req, res) {
-  res.send('Hello weibo')
+  fs.readFile('/weibo/index.txt', function(err,res) {
+    res.send('data:',res)    
+  })
 })
 app.listen(7100, function(){
         console.log('listening on port 7100')
